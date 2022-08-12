f4cde9979ba7389d7219eb414dc2f189767e3ee6
oh <outputor777@gmail.com>
2022-08-12 19:26:38 +0900

    button component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import Button from './elements/Button';
 
 function App() {
-  return (
-    <div>
-      <Button />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/elements/Button.jsx
+++ src/elements/Button.jsx
@@ -1,14 +1,63 @@
 import React from 'react'
-import styled, {css} from 'styled-components'
+import styled, { css } from 'styled-components'
 
-const Button = ({disabled, children}) => {
+const SIZES = {
+  sm: css`
+    --button-font-size: 0.875rem;
+    --button-padding: 8px 12px;
+    --button-radius: 4px;
+  `,
+  md: css`
+    --button-font-size: 1rem;
+    --button-padding: 12px 16px;
+    --button-radius: 8px;
+  `,
+  lg: css`
+    --button-font-size: 1.25rem;
+    --button-padding: 16px 20px;
+    --button-radius: 12px;
+  `,
+}
+
+const VARIANTS = {
+  success: css`
+    --button-color: #ffffff;
+    --button-bg-color: #28a745;
+    --button-hover-bg-color: #218838;
+  `,
+  error: css`
+    --button-color: #ffffff;
+    --button-bg-color: #dc3545;
+    --button-hover-bg-color: #c82333;
+  `,
+  warning: css`
+    --button-color: #212529;
+    --button-bg-color: #ffc107;
+    --button-hover-bg-color: #e0a800;
+  `,
+};
+
+
+const Button = ({disabled, size, variants, children}) => {
+  const sizeStyle = SIZES[size]
+  const variantStyle = VARIANTS[variants]
+
   return (
-    <StyledButton disabled={disabled}>{children}</StyledButton>
+    <StyledButton
+      disabled={disabled} 
+      sizeStyle={sizeStyle} 
+      variantStyle={variantStyle}
+    >
+      {children}
+    </StyledButton>
   )
 }
 
 
 const StyledButton = styled.button`
+  ${(props) => props.sizeStyle}
+  ${(props) => props.variantStyle}
+
   margin:0;
   border:none;
   cursor: pointer;
