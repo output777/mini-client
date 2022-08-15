2dce404fb37c2fbcfbaec2445bdaf156784f9b41
oh <outputor777@gmail.com>
2022-08-16 01:23:51 +0900

    test

--- src/redux/modules/campSlice.jsx
+++ src/redux/modules/campSlice.jsx
@@ -7,10 +7,12 @@ const initialState = {
   error: null,
 };
 
-export const __getCamps = createAsyncThunk('getCamps', async (payload, thunkAPI) => {
+
+
+export const __getCamps = createAsyncThunk('getCamps', async (_, thunkAPI) => {
   try {
     const data = await axios.get('http://localhost:3001/camps')
-    console.log('data', data);
+    // console.log('data', data);
     return thunkAPI.fulfillWithValue(data.data);
   } catch (error) {
     return thunkAPI.rejectWithValue(error)
@@ -20,18 +22,31 @@ export const __getCamps = createAsyncThunk('getCamps', async (payload, thunkAPI)
 export const __addCamps = createAsyncThunk('addCamp', async (payload, thunkAPI) => {
   try {
     const data = await axios.post('http://localhost:3001/camps', payload);
-    console.log('data', data);
+    // console.log('data', data);
     return thunkAPI.fulfillWithValue(data.data);
   } catch (error) {
     return thunkAPI.rejectWithValue(error)
   }
 })
 
+export const __deleteCamp = createAsyncThunk("deleteCamp", async (payload, thunkAPI) => {
+  try {
+    const data = await axios.delete(`http://localhost:3001/camps/${payload}`);
+    console.log(payload)
+    return thunkAPI.fulfillWithValue(payload);
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error);
+  }
+}
+);
+
+
 const campSlice = createSlice({
   name: 'camps',
   initialState,
   reducers: {},
   extraReducers: {
+
     // getCamps
     [__getCamps.pending]: (state) => {
       state.isLoading = true;
@@ -55,7 +70,23 @@ const campSlice = createSlice({
     [__addCamps.rejected]: (state, action) => {
       state.isLoading = true;
       state.error = action.payload;
-    }
+    },
+    //  deleteCamp
+    [__deleteCamp.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [__deleteCamp.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      console.log(state.camps)
+      console.log(action)
+      state.camps = state.camps.filter(camp => camp.id !== action.payload)
+    },
+    [__deleteCamp.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+
+
   }
 });
 
